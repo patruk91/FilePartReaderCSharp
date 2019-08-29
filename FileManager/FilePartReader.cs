c8377f784a9bacace554753ab44cb2817cdffe2c
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-29 16:30:23 +0200

    Initialize new repository

---
+++ FileManager/FilePartReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManager
+{
+    public class FilePartReader
+    {
+
+    }
+}
---
+++ FileManager/FileWordAnalyzer.cs
@@ -0,0 +1,7 @@
+﻿namespace FileManager
+{
+    public class FileWordAnalyzer
+    {
+        
+    }
+}
