2d3f53d52c9e8ffcb5859b3a0117ccdcb5fb9c38
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-29 16:31:13 +0200

    Setup initial parameters: filePath, fromLine and toLine

--- FileManager/FilePartReader.cs
+++ FileManager/FilePartReader.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FileManager
 {
     public class FilePartReader
     {
+        private string _filePath;
+        private int? _fromLine;
+        private int? _toLine;
 
+        public FilePartReader()
+        {
+            _filePath = "";
+            _fromLine = null;
+            _toLine = null;
+        }
+
+        public void Setup(string filePath, int fromLine, int toLine)
+        {
+            if (toLine < fromLine) throw new ArgumentException(
+                message: "Invalid argument: fromLine is bigger than toLine");
+            if (fromLine < 1) throw new ArgumentException(
+                message: "Invalid argument: fromLine is less than one");
+            this._filePath = filePath;
+            this._fromLine = fromLine;
+            this._toLine = toLine;
+        }
     }
 }
