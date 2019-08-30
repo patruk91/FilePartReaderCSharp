a617c893c9f69d120b525ea95801c7facde137f2
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-30 08:24:08 +0200

    Restructure project, add test solution for project

---
+++ FileManagerTest/FilePartReaderTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using FileManager;
+using NUnit.Framework;
+
+namespace FileManagerTest
+{
+    [TestFixture]
+    public class FilePartReaderTest
+    {
+
+    }
+}
---
+++ FileManagerTest/FileWordAnalyzerTest.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace FileManagerTest
+{
+    public class FileWordAnalyzerTest
+    {
+
+    }
+}
