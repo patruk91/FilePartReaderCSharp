23a0c337c16a16519734aecde64dfe8a2ef28b75
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-30 08:53:30 +0200

    Add tests for setup method

--- FileManagerTest/FilePartReaderTest.cs
+++ FileManagerTest/FilePartReaderTest.cs
@@ -8,6 +8,36 @@ namespace FileManagerTest
     [TestFixture]
     public class FilePartReaderTest
     {
+        private FilePartReader _filePartReader;
 
+        [SetUp]
+        public void Setup()
+        {
+            this._filePartReader = new FilePartReader();
+        }
+
+        [Test]
+        public void ThrowArgumentExceptionAtDefaultSetup()
+        {
+            Assert.Throws<ArgumentException>(() => _filePartReader.Read());
+        }
+
+        [Test]
+        public void ThrowArgumentExceptionIfFromLineIsBiggerThanToLine()
+        {
+            string filePath = @"F:\C#\PROJECTS\FilePartReader";
+            int fromLine = 4;
+            int toLine = 1;
+            Assert.Throws<ArgumentException>(() => _filePartReader.Setup(filePath, fromLine, toLine));
+        }
+
+        [Test]
+        public void ThrowArgumentExceptionIfFromLineIsLessThanOne()
+        {
+            string filePath = @"F:\C#\PROJECTS\FilePartReader";
+            int fromLine = 0;
+            int toLine = 4;
+            Assert.Throws<ArgumentException>(() => _filePartReader.Setup(filePath, fromLine, toLine));
+        }
     }
 }
