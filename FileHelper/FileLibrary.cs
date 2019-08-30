19d016ef4070c47f90bd5263c8809df28f1e7965
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-30 07:19:46 +0200

    Add extension method to reverse string

---
+++ FileHelper/FileLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace FileHelper
+{
+    public static class FileLibrary
+    {
+        public static string Reverse(this string str)
+        {
+            return str.ToCharArray().Reverse().ToString();
+        }
+    }
+}
