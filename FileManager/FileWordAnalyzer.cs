5351a24fd94ac761f4af0cbd31dd4ec336676c63
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-30 07:23:06 +0200

    Get words in array from file

--- FileManager/FileWordAnalyzer.cs
+++ FileManager/FileWordAnalyzer.cs
@@ -1,7 +1,22 @@
-﻿namespace FileManager
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileManager
 {
     public class FileWordAnalyzer
     {
-        
+        private FilePartReader _filePartReader;
+
+        public FileWordAnalyzer(FilePartReader filePartReader)
+        {
+            _filePartReader = filePartReader;
+        }
+
+        public string[] GetWords()
+        {
+            string content = _filePartReader.ReadLines();
+            return content.Split(separator: " ");
+        }
     }
 }
